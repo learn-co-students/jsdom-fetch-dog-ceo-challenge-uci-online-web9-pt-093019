62483eb33235f8db0acbe5530adc84b92438f7d0
Alice Freda <alicefreda@gmail.com>
2020-05-05 18:38:50 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-
+// document.addEventListener('DomContentLoaded', onLoad)
+//
+// function onLoad() {
+//    fetchImages();
+//    fetchBreeds();
+// }
   document.addEventListener('DOMContentLoaded', function() {
     fetchImages();
     fetchBreeds();
+    // add event listener for select breeds
+    const breedSelect = document.getElementById('breed-dropdown')
+    breedSelect.addEventListener('change', filterBreeds)
   })
 
 function fetchImages() {
     fetch("https://dog.ceo/api/breeds/image/random/4")
     .then(resp => resp.json())
-    .then(result => renderImage(result.message))
-  }
+    .then(json => renderImage(json.message))
+}
   
-  function renderImage(result) {
+  function renderImage(json) {
     const imageContainer = document.getElementById('dog-image-container')
-    result.forEach(imageURL => {
+    json.forEach(imageURL => {
         const img = document.createElement('img')
         img.src = imageURL
         imageContainer.appendChild(img)
@@ -23,17 +31,21 @@ function fetchImages() {
     const breedUrl = 'https://dog.ceo/api/breeds/list/all';
     fetch(breedUrl)
     .then(resp => resp.json())
-    .then(result => renderBreeds(result.message))
+    .then(json => renderBreeds(json.message))
   }
 
-  function renderBreeds(result) {
+  function renderBreeds(json) {
     const breedList = document.getElementById("dog-breeds");
-    for (const [key, value] of Object.entries(result)) {
+    // lookup for...in
+
+    for (const [key, value] of Object.entries(json)) {
         const li = document.createElement('li')
+        // add class to li to reference later
         if(value) {
             li.innerText = `${value} ${key}`
             breedList.appendChild(li)
             li.addEventListener('click', changeColor);
+            
         } else {
             li.innerText = key;
             breedList.appendChild(li)
@@ -45,4 +57,15 @@ function fetchImages() {
   function changeColor(event) {
     event.target.style.color = 'deeppink';
   }
-  
+ 
+  function filterBreeds(event) {
+    const userSelection = event.target.value
+    const breedList = document.getElementsByTagName('li')
+    for (const breed of breedList) {
+      if (breed.innerText.startsWith(userSelection)) {
+          breed.style.display = ""
+      } else {
+            breed.style.display = "none"
+      }
+    }
+  }
